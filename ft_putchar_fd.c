3e2a7904a12c34a26ec9ae9b75dc1dc8433f3535
juggorr <juggorrvol.2@gmail.com>
2023-11-28 20:41:27 +0900

    [WIP] fd funcs

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,12 +6,20 @@
 /*   By: juggorr <juggorr@gmail.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 19:21:29 by juggorr           #+#    #+#             */
-/*   Updated: 2023/11/23 19:22:51 by juggorr          ###   ########.fr       */
+/*   Updated: 2023/11/28 20:41:08 by juggorr          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
 
 void	ft_putchar_fd(char c, int fd)
 {
+	if (fd < 0)
+		return ;
+	write(1, &c, fd);
+}
 
+int	main(void)
+{
+	ft_putchar_fd('1', -1);
+	return (0);
 }
