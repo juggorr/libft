22ce1adde53ba1cd4f5e81103e9fca5bd662dc60
Jungho Park <junghopa@c6r4s8.42seoul.kr>
2023-12-07 18:02:53 +0900

    [DEF] 0

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: junghopa <juhnhopa@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 17:57:38 by junghopa          #+#    #+#             */
-/*   Updated: 2023/12/05 19:11:55 by juggorr          ###   ########.fr       */
+/*   Updated: 2023/12/07 17:44:39 by junghopa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 extern unsigned int	ft_strlen(const char *s);
--- ft_substr.c
+++ ft_substr.c
@@ -6,12 +6,13 @@
 /*   By: juggorr <juggorr@gmail.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 13:29:49 by juggorr           #+#    #+#             */
-/*   Updated: 2023/12/07 16:16:08 by juggorr          ###   ########.fr       */
+/*   Updated: 2023/12/07 18:00:08 by junghopa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 extern unsigned int	ft_strlen(const char *s);
+extern char			*ft_strdup(const char *s);
 
 char	*ft_substr(char const *s, size_t start, size_t len)
 {
@@ -20,11 +21,16 @@ char	*ft_substr(char const *s, size_t start, size_t len)
 	size_t	src_len;
 
 	idx = 0;
+	src_len = ft_strlen(s);
+	if (start > src_len)
+		return (ft_strdup(""));
+	if (len > src_len)
+		len = src_len - start;
 	str = (char *)malloc(sizeof(char) * (len + 1));
 	if (!str)
 		return (0);
 	src_len = ft_strlen(s);
-	while (idx < len && start < src_len)
+	while (idx < len && start < src_len && s[start])
 	{
 		str[idx] = s[start];
 		idx++;
