58e053e5b1f3a2d2172564351d40a6cd576d3c85
juggorr <juggorrvol.2@gmail.com>
2023-12-12 15:41:29 +0900

    [WAN] need refactoring

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+#include "libft.h"
+
+int	main()
+{
+	//printf("%lu\n", ft_strlen(0));
+	printf("%lu\n", strlen(0));
+}
